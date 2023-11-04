acd56d1b160bd8db96d3854de5f445454010113e
Rico <96888953+KopfdesDaemons@users.noreply.github.com>
2023-11-04 16:28:23 +0100

    mobile optimization

--- page.php
+++ page.php
@@ -7,7 +7,7 @@ get_header();
         while (have_posts()) {
             the_post();
         ?>
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <article id="post-<?php the_ID(); ?>" <?php post_class('clean_space_content_container'); ?>>
                 <h1><?php the_title(); ?></h1>
                 <?php the_content(); ?>
             </article>
